a718b0976e01038ec73560e5fb8a609932252a34
Julio Reyes <juliob10.jr@gmail.com>
2026-03-20 16:03:41 -0600

    Write a program that displays your name, address, city, state, zip, phone number and college major

---
+++ Chapter_02/exercise_02_01.py
@@ -0,0 +1,20 @@
+# Starting Out with Python, Third Edition
+# Capítulo 2, Ejercicio 1
+#Write a program that displays your name, address, city, state, zip, phone number and college major
+
+def print_personal_info():
+    print("Name: Julio Reyes")
+    print("Address: Molino de la Mancha 32")
+    print("City: Zapopan")
+    print("State: Zapopan")
+    print("ZIP: 45200")
+    print("Phone: 3312742713")
+    print("AI and Python")
+
+
+def main():
+    print_personal_info()
+    pass
+
+if __name__ == "__main__":
+    main()
